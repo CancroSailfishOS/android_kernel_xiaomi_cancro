63a25b6981c4dc45804bcf7a4e44be48bb09af9a
Alireza Barani <alireza1219@live.com>
2019-04-20 17:52:27 -0400

    Fixing my mess up :)

--- include/linux/netfilter_ipv6/ip6_tables.h
+++ include/linux/netfilter_ipv6/ip6_tables.h
@@ -287,7 +287,16 @@ extern unsigned int ip6t_do_table(struct sk_buff *skb,
 				  struct xt_table *table);
 
 /* Check for an extension */
-extern int ip6t_ext_hdr(u8 nexthdr);
+static inline int
+ip6t_ext_hdr(u8 nexthdr)
+{	return (nexthdr == IPPROTO_HOPOPTS) ||
+	       (nexthdr == IPPROTO_ROUTING) ||
+	       (nexthdr == IPPROTO_FRAGMENT) ||
+	       (nexthdr == IPPROTO_ESP) ||
+	       (nexthdr == IPPROTO_AH) ||
+	       (nexthdr == IPPROTO_NONE) ||
+	       (nexthdr == IPPROTO_DSTOPTS);
+}
 
 enum {
 	IP6T_FH_F_FRAG	= (1 << 0),
